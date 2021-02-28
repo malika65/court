2ff03528416248073b132333b57e95878ced3a67
darika666 <begalievad@gmail.com>
2021-02-28 20:53:07 +0600

    quill2

--- src/smi/models.py
+++ src/smi/models.py
@@ -3,9 +3,22 @@ from django_quill.fields import QuillField
 
 # Create your models here.
 
-class Post(models.Model):
-    body = QuillField()
-
 class Tag(models.Model):
-    tags = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = 'Теги'
+        verbose_name_plural = 'Теги'
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200,verbose_name = 'Заголовок')
+    body = QuillField(verbose_name = 'Тело поста')
+    tags = models.ManyToManyField(Tag, related_name = 'post', verbose_name = 'Теги')
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+    def __str__(self):
+        return self.title
 
