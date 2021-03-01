1a853e2bceaa6e638529809b985f950810300739
darika666 <begalievad@gmail.com>
2021-03-01 14:43:45 +0600

    add vacancy model

--- src/smi/admin.py
+++ src/smi/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from .models import Post, Tag
+from .models import Post, Tag, Vacancy
 
 # Register your models here.
 
 # admin.site.register(Post)
 admin.site.register(Tag)
+admin.site.register(Vacancy)
 
 @admin.register(Post)
 class QuillPostAdmin(admin.ModelAdmin):
     pass
+
+
 
